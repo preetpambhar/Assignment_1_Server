85828aa8bf00c33215c5edf077956af0ebe6fc1b
Preet-Patel <70093554+Preet-Patel@users.noreply.github.com>
2024-04-11 03:27:39 -0400

    feat: product api

--- index.js
+++ index.js
@@ -21,8 +21,23 @@ const userSchema = new mongoose.Schema({
 });
 
 const User = mongoose.model("User", userSchema);
-// User routes
 
+// Define product schema
+const productSchema = new mongoose.Schema({
+  img: String,
+  title: String,
+  star: String, // Assuming you want to store the star as a string
+  reviews: String,
+  prevPrice: String,
+  newPrice: String,
+  company: String,
+  color: String,
+  category: String,
+  shipping_cost: Number,
+});
+const Product = mongoose.model("Product", productSchema);
+
+// User routes
 // Sign Up new user
 app.post("/signup", async (req, res) => {
   try {
@@ -61,6 +76,90 @@ app.post("/signin", async (req, res) => {
     res.send("Sign in successful");
   } catch (error) {
     res.status(500).send("Error signing in");
+  }
+});
+
+// Create a new product
+app.post("/products", async (req, res) => {
+  try {
+    const product = new Product(req.body);
+    await product.save();
+    res.status(201).send(product);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+// Get all products
+app.get("/products", async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.send(products);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+// Get a product by ID
+app.get("/products/:id", async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+      return res.status(404).send("Product not found");
+    }
+    res.send(product);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+// Update a product by ID
+app.patch("/products/:id", async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = [
+    "img",
+    "title",
+    "star",
+    "reviews",
+    "prevPrice",
+    "newPrice",
+    "company",
+    "color",
+    "category",
+    "shipping_cost",
+  ];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "Invalid updates!" });
+  }
+
+  try {
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!product) {
+      return res.status(404).send("Product not found");
+    }
+    res.send(product);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+// Delete a product by ID
+app.delete("/products/:id", async (req, res) => {
+  try {
+    const product = await Product.findByIdAndDelete(req.params.id);
+    if (!product) {
+      return res.status(404).send("Product not found");
+    }
+    res.send(product);
+  } catch (error) {
+    res.status(500).send(error);
   }
 });
 
